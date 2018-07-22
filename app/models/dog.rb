b6c12bb303b782e6a0de94033d880ca830610717
Christopher Wood <cjwood032@gmail.com>
2018-07-22 16:48:36 +0000

    automatically backed up by learn

---
+++ app/models/dog.rb
@@ -0,0 +1,9 @@
+Class Dog
+  attr_accessor :name, :breed, :age
+  @@all=[]
+  def initialize(name=nil, breed=nil,age=nil)
+    @name = name
+    @breed = breed
+    @age = age
+    @@all << self
+  end
